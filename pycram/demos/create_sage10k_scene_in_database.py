3a45ad9a2f6c6151554a9a3fe286f6dcfac42112
Tom Schierenbeck <tom_sch@uni-bremen.de>
2026-05-07 13:04:46 +0200

    [SemDT] pre junie

--- pycram/demos/create_sage10k_scene_in_database.py
+++ pycram/demos/create_sage10k_scene_in_database.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from krrood.ormatic.data_access_objects.helper import to_dao
 from krrood.ormatic.utils import create_engine, drop_database
 from pycram.sage_10k.demos import Sage10kSouthwesternStoreDemo
+from semantic_digital_twin.adapters.sage_10k_dataset.utils import create_hsrb_in_world
 from semantic_digital_twin.orm.ormatic_interface import *
 
 
@@ -26,15 +27,7 @@ def main():
     print("loading scene")
     demo = Sage10kSouthwesternStoreDemo()
     demo.create_world()
-    # loader = Sage10kDatasetLoader()
-    # scene = loader.create_scene(Sage10kNonShittyScenes.SOUTHWESTERN_STORE)
-    # world = scene.create_world()
     print(f"Loading the scene took {time.time() - current_time:.2f} seconds")
-
-    current_time = time.time()
-    print("loading robot")
-    # pr2 = create_hsrb_in_world(world)
-    print(f"Loading the robot took {time.time() - current_time:.2f} seconds")
 
     current_time = time.time()
     print("saving to database")
