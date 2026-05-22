f83e88110f0feb7f9859b779717d94c5122b2f44
David Prüser <david@pruesernet.de>
2026-05-22 11:37:33 +0200

    [SemDT] typo

--- semantic_digital_twin/src/semantic_digital_twin/robots/pr2.py
+++ semantic_digital_twin/src/semantic_digital_twin/robots/pr2.py
@@ -61,7 +61,7 @@ class PR2(AbstractRobot, SpecifiesLeftRightArm, HasNeck):
 
     def _setup_collision_rules(self):
         """
-        eoads the SRDF file for the PR2 robot, if it exists.
+        Loads the SRDF file for the PR2 robot, if it exists.
         """
         srdf_path = os.path.join(
             Path(files("semantic_digital_twin")).parent.parent,
